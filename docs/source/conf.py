978230379e017267852e0b233fdfbc7d2f23da12
Joseph Hand <jhand@blpc0.bl.pvt>
2023-06-21 09:55:19 -0700

    Remove intersphinx.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,15 +20,8 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
 	'sphinx.ext.autodoc',
-	'sphinx.ext.autosummary',
-	'sphinx.ext.intersphinx'
+	'sphinx.ext.autosummary'
 ]
-
-intersphinx_mapping = {
-	'python': ('https:/docs.python.org/3/', None),
-	'sphinx': ('https://www.sphinx-doc.org/en/master/', None)
-}
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 exclude_patterns = ['**/.ipynb_checkpoints']

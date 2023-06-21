7b047835498f1c670767c0804152cb834afacfb3
Joseph Hand <jhand@blpc0.bl.pvt>
2023-06-21 12:44:19 -0700

    Add license information.

--- alias/continuum_normalization.py
+++ alias/continuum_normalization.py
@@ -1,3 +1,19 @@
+# (C) 2023 Joseph Hand.
+#
+# This file is part of ALIAS.
+#
+# ALIAS is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# ALIAS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# ALIAS. If not, see <https://www.gnu.org/licenses/>. 
+
 '''Small library for continuum normalizing APOGEE spectra'''
 
 import numpy as np
